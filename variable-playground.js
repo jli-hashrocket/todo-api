21927f65ab2d0ac9e26ce83979501ebe1fa2f913
macheteshuffle <jeff.li029@gmail.com>
2016-08-21 18:37:50 -0400

    passing variables by reference in javascript

---
+++ variable-playground.js
@@ -0,0 +1,22 @@
+var person = {
+  name: 'Jeff',
+  age: 21
+};
+
+function updatePerson(obj){
+  obj.age = 35;
+}
+
+updatePerson(person);
+console.log(person);
+
+//Array Example
+var grades = [15,37];
+
+function addGrade(grades){
+  grades.push(78);
+  debugger;
+}
+
+addGrade(grades);
+console.log(grades);
