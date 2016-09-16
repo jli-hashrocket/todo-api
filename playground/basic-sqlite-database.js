71891597bc41601907625a344be952c16e639b58
macheteshuffle <jeff.li029@gmail.com>
2016-09-15 21:14:01 -0400

    testing db associations

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,41 +19,34 @@ var Todo = sequelize.define('todo', {
   }
 });
 
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
   // force: true
 }).then(function () {
   console.log('Everything is synced');
-  Todo.findById(3).then( function (todo) {
-    if (todo) {
-      console.log(todo.toJSON());
-    } else {
-      console.log('Todo not found');
-    }
+
+  User.findById(1).then(function (user) {
+    user.getTodos({where: {completed: false}}).then(function (todos) {
+      todos.forEach(function (todo) {
+        console.log(todo.toJSON());
+      });
+    });
   });
-  // Todo.create({
-  //   description: 'Finish course',
-  //   completed: false
-  // }).then(function (todo){
+  // User.create({
+  //   email: 'jeff.li@test.com'
+  // }).then(function () {
   //   return Todo.create({
-  //     description: 'Clean room'
+  //     description: 'Clean yard'
   //   });
-  // }).then(function(){
-  //   return Todo.findAll({
-  //     where: {
-  //       description: {
-  //         $like: '%finish%'
-  //       }
-  //     }
+  // }).then(function (todo) {
+  //   User.findById(1).then(function (user) {
+  //     user.addTodo(todo);
   //   });
-  // }).then(function(todos){
-  //   if (todos) {
-  //     todos.forEach(function (todo) {
-  //       console.log(todo.toJSON());
-  //     });
-  //   } else {
-  //     console.log('no todos found');
-  //   }
-  // }).catch(function (e){
-  //   console.log(e);
   // });
 });
